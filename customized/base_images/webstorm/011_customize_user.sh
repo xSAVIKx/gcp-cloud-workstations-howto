9bc07b01544ae896283f85d34b221738bdd8f83c
Yurii Serhiichuk <savik.ne@gmail.com>
2025-11-11 20:15:56 +0100

    Helm fix is no longer relevant. new images have same source repo by default

--- customized/base_images/webstorm/011_customize_user.sh
+++ customized/base_images/webstorm/011_customize_user.sh
@@ -2,7 +2,7 @@
 
 sudo mkdir -p /home/user/.workstation/
 sudo mkdir -p /home/user/.local/bin/
-sudo cp /tmp/travelshift/customize_environment.sh /home/user/.workstation/customize_environment
+sudo cp /tmp/customize_environment.sh /home/user/.workstation/customize_environment
 
 sudo chown -R user /home/user/
 sudo chmod +x /home/user/.workstation/customize_environment
