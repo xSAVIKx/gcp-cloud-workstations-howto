662b7a7a79e6fdb4e38968b7298a824081ba2802
Yurii Serhiichuk <savik.ne@gmail.com>
2026-03-09 22:50:59 +0100

    Update dependencies versions

--- customized/base_images/webstorm/customize_environment.sh
+++ customized/base_images/webstorm/customize_environment.sh
@@ -2,8 +2,8 @@
 
 set -x;
 export DEBIAN_FRONTEND=noninteractive
-export PULUMI_VERSION="3.208.0"
-export CLOUD_SQL_PROXY_VERSION="2.19.0"
+export PULUMI_VERSION="3.225.0"
+export CLOUD_SQL_PROXY_VERSION="2.21.1"
 
 curl -fsSL https://get.pulumi.com | bash -s -- --version "${PULUMI_VERSION}"
 
